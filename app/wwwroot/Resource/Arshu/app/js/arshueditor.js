62bc590c94b39a33d71a685e0da583f0be120c44
Sridharan Srinivasan <srid68@gmail.com>
2024-05-29 10:49:26 +0800

    wwwroot updated

--- app/wwwroot/Resource/Arshu/app/js/arshueditor.js
+++ app/wwwroot/Resource/Arshu/app/js/arshueditor.js
@@ -5,6 +5,156 @@ let gJsonEditorItemsPerPage = 5;
 let gJsonEditorItemsPerPageArray = 2;
 let gJsonEditorItemsPerPageArrayItem = 2;
 
+function fillJsonUpdate(viewId, progressElmId, responseElmId, jsonSourceAppSite, jsonSourceAppView, jsonSource, jsonKey, targetElmId, showElmId, pageNo, pageNoArray, pageNoArrayItem, filterKeyPath, successCallback) {
+    let id = window.ajaxid++;
+
+    let responseContainerElmId = responseElmId + "Container";
+    let responseElm = getElm(responseElmId)
+    let responseContainerElm = getElm(responseContainerElmId)
+    //while (responseElm.firstChild) responseElm.removeChild(responseElm.firstChild);
+    //hideElm(responseContainerElmId);
+
+    let valid = true;
+
+    let itemsPerPage = gJsonEditorItemsPerPage;
+    if (haveElm(viewId + "JsonEditorItemsPerPage") == true) {
+        itemsPerPage = getElm(viewId + "JsonEditorItemsPerPage").value;
+        gJsonEditorItemsPerPage = itemsPerPage;
+    }
+
+    let itemsPerPageArray = gJsonEditorItemsPerPageArray;
+    if (haveElm(viewId + filterKeyPath + "jsonArrayEditorItemsPerPage") == true) {
+        itemsPerPageArray = getElm(viewId + filterKeyPath + "jsonArrayEditorItemsPerPage").value;
+        gJsonEditorItemsPerPageArray = itemsPerPageArray;
+    }
+
+    let itemsPerPageArrayItem = gJsonEditorItemsPerPageArrayItem;
+    if (haveElm(viewId + filterKeyPath + "jsonArrayItemEditorItemsPerPage") == true) {
+        itemsPerPageArrayItem = getElm(viewId + filterKeyPath + "jsonArrayItemEditorItemsPerPage").value;
+        gJsonEditorItemsPerPageArrayItem = itemsPerPageArrayItem;
+    }
+
+    if (targetElmId.trim().length == 0) {
+        showText(responseElm, 'Enter Target Element Id', 'red');
+        valid = false;
+    }
+
+    if (showElmId.trim().length == 0) {
+        showText(responseElm, 'Enter Show Element Id', 'red');
+        valid = false;
+    }
+
+
+    if (valid === true) {
+
+        let url = '/AppApi/VueApi';
+        let urlMethod = 'POST';
+        let urlContent = {
+            "jsonrpc": '2.0', "method": 'GetJsonUpdateView',
+            "params": {
+                "viewId": viewId,
+                "jsonAppSite": jsonSourceAppSite,
+                "jsonAppView": jsonSourceAppView,
+                "jsonSource": jsonSource,
+                "jsonKey": jsonKey,
+                "pageNo": pageNo,
+                "itemsPerPage": itemsPerPage,
+                "pageNoArray": pageNoArray,
+                "itemsPerPageArray": itemsPerPageArray,
+                "pageNoArrayItem": pageNoArrayItem,
+                "itemsPerPageArrayItem": itemsPerPageArrayItem,
+                "filterKeyPath": filterKeyPath
+            },
+            "id": id
+        };
+
+        dopost(progressElmId, responseElmId, url, urlContent,
+            function (data) {
+                if ((data.hasOwnProperty('Result') === true) && (data.Result)) {
+                    let result = data.Result;
+
+                    if (result.hasOwnProperty('message') === true) {
+                        //showText(responseElm, result.message, 'green');
+                        if (result.hasOwnProperty('html') === true) {
+
+                            if (filterKeyPath.length == 0) {
+                                let rootNode = getElm(targetElmId);
+                                rootNode.innerHTML = result.html;
+                                showElm(showElmId);
+                            } else {
+                                let rootNode = getElm(targetElmId);
+                                rootNode.outerHTML = result.html;
+                                showElm(showElmId);
+                            }
+
+                            if (typeof successCallback === "function") {
+                                successCallback();
+                            }
+                        }
+                        else if (result.hasOwnProperty('json') === true) {
+                            let json = result.json;
+                            //fillProfileRole(json);
+
+                            if (typeof successCallback === "function") {
+                                successCallback();
+                            }
+                        }
+                    }
+                    else if (result.hasOwnProperty('error') === true) {
+                        showText(responseElm, result.error, 'red');
+                    }
+                }
+                else if ((data.hasOwnProperty('Error') === true) && (data.Error)) {
+                    let error = data.Error;
+                    if (error.hasOwnProperty('Message') === true) {
+                        showText(responseElm, error.Message, 'red');
+                    }
+                }
+            });
+
+    }
+    return false;
+}
+
+function refreshUpdateJson(viewId, progressElmId, pageNo, pageNoArray, pageNoArrayItem, editorResponseElmId, editorTargetElmId, editorShowElmId, successCallback) {
+    let valid = true;
+    if (valid == true) {
+
+        let responseElmId = viewId + 'JsonEditorResponse';
+        let responseContainerElmId = responseElmId + "Container";
+        let responseElm = getElm(responseElmId)
+        let responseContainerElm = getElm(responseContainerElmId)
+        while (responseElm.firstChild) responseElm.removeChild(responseElm.firstChild);
+        hideElm(responseContainerElmId);
+
+        let jsonEditorTargetElmId = viewId;
+        let jsonEditorShowElmId = viewId + "JsonEditorSection";
+
+        if (editorResponseElmId != "") {
+            if (haveElm(editorResponseElmId) == true) {
+                jsonEditorResponseElmId = editorResponseElmId;
+            }
+        }
+        if (editorTargetElmId != "") {
+            if (haveElm(editorTargetElmId) == true) {
+                jsonEditorTargetElmId = editorTargetElmId;
+            }
+        }
+        if (editorShowElmId != "") {
+            if (haveElm(editorShowElmId) == true) {
+                jsonEditorShowElmId = editorShowElmId;
+            }
+        }
+
+        let jsonSourceAppSite = getElm(viewId + 'EditorJsonSourceAppSite').value;
+        let jsonSourceAppView = getElm(viewId + 'EditorJsonSourceAppView').value;
+        let jsonSource = getElm(viewId + 'EditorJsonSource').value;
+        let jsonKey = "";
+
+        fillJsonUpdate(viewId, progressElmId, responseElmId, jsonSourceAppSite, jsonSourceAppView, jsonSource, jsonKey, jsonEditorTargetElmId, jsonEditorShowElmId, pageNo, pageNoArray, pageNoArrayItem, "", successCallback);
+    }
+}
+
 function fillJsonEditor(viewId, progressElmId, responseElmId, jsonSourceAppSite, jsonSourceAppView, jsonSource, jsonKey, viewModeVal, targetElmId, showElmId, pageNo, pageNoArray, pageNoArrayItem, filterKeyPath, successCallback) {
     let id = window.ajaxid++;
 
@@ -230,20 +380,26 @@ function saveEditorJson(viewId, progressElmId,  getUpdateJson, refreshComponentN
     if (typeof getUpdateJson === "function") {
         flatJsonData = getUpdateJson();
     }
-    let flatJsonDataText = JSON.stringify(flatJsonData);
 
-    if (typeof successCallback === "function") {
-        saveJson(progressElmId, responseElmId,
-            jsonSourceAppSite, jsonSourceAppView, jsonSource,
-            flatJsonDataText,
-            refreshComponentName, {}, false, '', '', successCallback);
+    const hasKeys = !!Object.keys(flatJsonData).length;
+    if (hasKeys ==true) {
+        let flatJsonDataText = JSON.stringify(flatJsonData);
+
+        if (typeof successCallback === "function") {
+            saveJson(progressElmId, responseElmId,
+                jsonSourceAppSite, jsonSourceAppView, jsonSource,
+                flatJsonDataText, flatJsonDataText,
+                refreshComponentName, {}, false, '', successCallback);
+        } else {
+            saveJson(progressElmId, responseElmId,
+                jsonSourceAppSite, jsonSourceAppView, jsonSource,
+                flatJsonDataText, flatJsonDataText,
+                refreshComponentName, {}, false, '', '', function () {
+                    refreshEditorJsonArray(viewId, progressElmId, pageNoCurrent, pageNoArray, keyPath, successCallback)
+                });
+        }
     } else {
-        saveJson(progressElmId, responseElmId,
-            jsonSourceAppSite, jsonSourceAppView, jsonSource,
-            flatJsonDataText,
-            refreshComponentName, {}, false, '', '', function () {
-                refreshEditorJsonArray(viewId, progressElmId, pageNoCurrent, pageNoArray, keyPath, successCallback)
-            });
+        showText(responseElm, 'No Changed Json Found to Save', 'red');
     }
 }
 

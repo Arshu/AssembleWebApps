c2c1d28cf4cd83d5cdd791ed3de7bbe2698f687e
Sridharan Srinivasan <srid68@gmail.com>
2024-10-30 22:28:09 +0800

    After Prune
    Former-commit-id: 7feae73eba52f9d74e996849afc53014d6747b1d

--- prune.bat
+++ prune.bat
@@ -1,4 +1,4 @@
-java -jar ..\bfg.jar --strip-blobs-bigger-than 10M .
+java -jar .\bfg.jar --strip-blobs-bigger-than 10M .
 
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
 
